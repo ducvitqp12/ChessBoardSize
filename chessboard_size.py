2db80a69f13dc698a8aacf106e6761f1b0a4cfa9
nguyenducdat157 <nguyenducdat157@gmail.com>
2023-02-07 16:57:14 +0700

    find columns and rows

--- chessboard_size.py
+++ chessboard_size.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+import math
 
 
 def remove_sin_wave_noise(image):
@@ -69,6 +70,18 @@ def line_detection(image):
 
     return lines_list
 
+def remove_duplicate(array):
+    result = [array[0]]
+    for ele in array:
+        check = True
+        for res in result:
+            if(np.abs(ele[1] - res[1]) < 30):
+                check = False
+                break
+        if check:
+            result.append(ele)
+
+    return result
 
 if __name__ == '__main__':
     # Đọc hình ảnh ở dạng xám
@@ -79,6 +92,8 @@ if __name__ == '__main__':
     # Line detect
     lines = line_detection(img)
 
+    array_object = []
+
     # Visualize
     imm = np.zeros((img.shape[0], img.shape[1], 3))
     for line in lines:
@@ -87,7 +102,48 @@ if __name__ == '__main__':
         x2, y2 = line[1]
         # Draw the lines joing the points
         # On the original image
+        Ox = [1, 0]
+        Oy = [0,1]
+        vector = [x2 - x1, y2 - y1]
+      
+        unit_vector_1 = Ox / np.linalg.norm(Ox)
+        unit_vector_2 = vector / np.linalg.norm(vector)
+        dot_product = np.dot(unit_vector_1, unit_vector_2)
+        angle = np.arccos(dot_product) if np.arccos(dot_product) <= math.pi / 2 else math.pi - np.arccos(dot_product) 
+        angle_deg = angle * 180 / math.pi 
+
+        # find distance
+        p1=np.array([x1,y1])
+        p2=np.array([x2,y2])
+        p3=np.array([0,0])
+        distance = np.linalg.norm(np.cross(p2-p1, p1-p3))/np.linalg.norm(p2-p1)
+        array_object.append([angle_deg, distance, line])
+        # print(angle * 180 / math.pi, end='\n')
+        
+
+    vertical = []
+    horizontal = []
+
+    for obj in array_object:
+        if(obj[0] < 45):
+            horizontal.append(obj)
+        else:
+            vertical.append(obj)
+
+    # filter duplicate line
+    print(len(remove_duplicate(vertical)), end='\n')
+    print(len(remove_duplicate(horizontal)))
+
+    verLine = remove_duplicate(horizontal)
+
+    for line in verLine:
+        x1, y1 = line[2][0]
+        x2, y2 = line[2][1]
         cv2.line(imm, (x1, y1), (x2, y2), (0, 255, 255), 2)
+    
+
+
+
 
     plt.imshow(imm, cmap="gray")
 
